da69e98490dcda3eb7026b831dd50e544ada8316
ThuanyDias <thuanydiasgomes@gmail.com>
2024-08-16 14:47:49 -0300

    mundando um alerta

--- Script.js
+++ Script.js
@@ -3,6 +3,10 @@ function generateNumber() {
     const min = Math.ceil(document.querySelector(".input-min").value)
     const max = Math.floor(document.querySelector(".input-max").value)
     
+  if(min >= max) {
+    alert ("o valor minimo tem que ser menor que o valor maximo")
+  }
+
     const result = Math.floor(Math.random() * (max - min) + min);
     console.log(min, max)
 
